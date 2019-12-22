8e2e0c4bbe777a22935a4a3a17e6012c027b348b
pavlyk <pawwlyk52@gmail.com>
2019-12-22 22:46:45 +0300

    env

---
+++ Specialisation Coursera/week1/command.py
@@ -0,0 +1,13 @@
+# unix
+# pip install requests
+# python3 -m venv env
+# source env/bin/activate
+# deactivate
+
+# windows
+# python3 -m venv c:\path\to\myenv
+# Где вместо c:\path\to\myenv укажите путь до папки с виртуальным окружением, которую вы хотите создать.
+# После того как скрипт отработает, вы можете активировать виртуальное окружение с помощью:
+# c:\path\to\myenv\Scripts\activate.bat
+# Чтобы деактивировать виртуальное окружение:
+# c:\path\to\myenv\Scripts\deactivate.bat
